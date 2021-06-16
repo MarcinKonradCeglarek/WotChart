83a5c35e2f54187418b1b674ea0d0cdb04c8b258
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2021-06-16 17:46:37 +0100

    Initial project

---
+++ wotchart/src/App.tsx
@@ -0,0 +1,74 @@
+// import React from "react";
+import Drawer from "./Navigation/Drawer";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+        <Drawer />
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ wotchart/src/Clan/models.ts
@@ -0,0 +1,62 @@
+enum ResponseStatus {
+    ok
+}
+
+interface ResponseMeta {
+    count: number;
+    total: number;
+}
+
+export interface Response {
+    status: ResponseStatus;
+    meta: ResponseMeta;
+}
+
+export interface ClanDetailsRequest {
+    application_id: string;
+    clan_id: string;
+}
+
+export interface ClanDetailsResponse extends ResponseMeta {
+    data: ClanDetails[];
+}
+
+interface ClanDetails {
+    accepts_join_requests: boolean;
+    clan_id: number;
+    color: 	string;
+    created_at:	Date;
+    creator_id: number;
+    creator_name: string;
+    description: string;
+    description_html: string;
+    is_clan_disbanded: boolean;
+    leader_id: number;
+    leader_name: string;
+    members_count: number;
+    motto: string;
+    name: string;
+    old_name: string;
+    old_tag: string;
+    renamed_at: Date;
+    tag: string;
+    updated_at: Date;
+    emblems: ClanDetailsEmblems;
+    members: ClanDetailsMember;
+}
+
+interface ClanDetailsEmblems {
+    x195: object;
+    x24: object;
+    x256: object;
+    x32: object;
+    x64: object;
+}
+
+interface ClanDetailsMember {
+    account_id:	number;
+    account_name: string;
+    joined_at: Date;
+    role: string;
+    role_i18n: string;
+}
